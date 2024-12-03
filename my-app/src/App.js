c52ec852357a7a0215f2b3c038632c1b12d8f99e
Lelox028 <giulianopascarelli@gmail.com>
2024-12-03 17:11:51 -0300

    added body

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,21 +3,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <div>
+      <img src= 'https://picsum.photos/200'/>
+      <h1>Giuliano Pascarelli</h1>
+      <p>Aca va mi descripcion</p>
+      <h3> Subtitulo xd</h3>
+      <p>parrafo explicando algo que deberia explicarse o al menos tener una explicacion</p>
+      <ul>
+        <li>coso1</li>
+        <li>coso2</li>
+      </ul>
     </div>
   );
 }
