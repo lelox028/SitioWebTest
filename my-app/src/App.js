e684c9b1dd21caf43fb131973cfa71012b15b20e
Lelox028 <giulianopascarelli@gmail.com>
2024-12-04 14:14:15 -0300

    added useStates and logic for changing values

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,20 +1,29 @@
 //import logo from './logo.svg';
 import './App.scss';
 import { Checkbox } from '@mui/material';
+import React, { FunctionComponent } from 'react'
 import ProfilePicUrl from "./dist/ProfilePic.JPG"
 
 
 function App() {
   let PersonalDescription = "Aca va mi descripcion"
-  const items = [
-    { name: 'Topbar', status: false },
-    { name: 'Texto que se actualice segun variables de React', status: true },
-    { name: 'Listas con checkbox', status: true },
-    { name: 'Una Database conectada.', status: false },
-    { name: 'Una foto mia (Opcional: salir presentable)', status: true },
-    { name: 'Links a mi github, linkedin, etc.', status: false },
-    { name: 'useStates', status: false }
-  ];
+  const [items, setitems] = React.useState([{ name: 'Topbar', status: false },
+  { name: 'Texto que se actualice segun variables de React', status: true },
+  { name: 'Listas con checkbox', status: true },
+  { name: 'Una Database conectada.', status: false },
+  { name: 'Una foto mia (Opcional: salir presentable)', status: true },
+  { name: 'Links a mi github, linkedin, etc.', status: false },
+  { name: 'useStates', status: true }
+  ])
+  
+  const toggleStatus = (name) => {
+    setitems((prevItems) =>
+      prevItems.map((item) =>
+        item.name === name ? { ...item, status: !item.status } : item
+      )
+    );
+  };
+
   return (
     <div className="Body">
       <div className="Main">
@@ -27,14 +36,15 @@ function App() {
           <h3> Subtitulo xd</h3>
           <p>Cosas que me gustaria que este sitio tenga:</p>
           <div className="UList">
-              {items.map((item) => (
-                <div className='CheckBox'>
-                  <Checkbox
-                    checked={item.status}
-                  />
-                  <p>{item.name}</p>
-                </div>
-              ))}
+            {items.map((item) => (
+              <div className='CheckBox'>
+                <Checkbox
+                  checked={item.status}
+                  onClick={(e) => { toggleStatus(item.name) }}
+                />
+                <p>{item.name}</p>
+              </div>
+            ))}
           </div>
         </div>
         <div className="BioSection">
